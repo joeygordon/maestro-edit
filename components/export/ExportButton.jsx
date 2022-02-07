53dcafd85ecad9628f17c0866707eafa042b0268
Joey Gordon <joey.gordon@gmail.com>
2022-02-06 16:59:45 -0800

    a modal is overkill

--- components/export/ExportButton.jsx
+++ components/export/ExportButton.jsx
@@ -1,33 +1,9 @@
 import { useState } from 'react';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
-import styled from 'styled-components';
 
 import generateChannel from './generateChannel';
 import generateGlobal from './generateGlobal';
-
-const PresetModal = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.6);
-
-  &.hidden {
-    display: none;
-  }
-`;
-
-const ModalContent = styled.div`
-  width: 400px;
-  padding: 2em;
-  background: white;
-  text-align: center;
-`;
 
 const ExportButton = ({ settings }) => {
   const [selectedPreset, setSelectedPreset] = useState('00');
@@ -74,41 +50,26 @@ const ExportButton = ({ settings }) => {
 
   return (
     <>
-      <button type='button' onClick={toggleModal}>
-        Export
+      <select id='preset' onChange={(e) => setSelectedPreset(e.target.value)}>
+        <option value='00'>00</option>
+        <option value='01'>01</option>
+        <option value='02'>02</option>
+        <option value='03'>03</option>
+        <option value='04'>04</option>
+        <option value='05'>05</option>
+        <option value='06'>06</option>
+        <option value='07'>07</option>
+        <option value='08'>08</option>
+        <option value='09'>09</option>
+        <option value='10'>10</option>
+        <option value='11'>11</option>
+        <option value='12'>12</option>
+        <option value='13'>13</option>
+      </select>
+      <br />
+      <button type='button' onClick={createZip}>
+        Export Preset
       </button>
-      <PresetModal className={modalVisible ? '' : 'hidden'}>
-        <ModalContent>
-          <div>Choose a preset slot</div>
-          <select
-            id='preset'
-            onChange={(e) => setSelectedPreset(e.target.value)}
-          >
-            <option value='00'>00</option>
-            <option value='01'>01</option>
-            <option value='02'>02</option>
-            <option value='03'>03</option>
-            <option value='04'>04</option>
-            <option value='05'>05</option>
-            <option value='06'>06</option>
-            <option value='07'>07</option>
-            <option value='08'>08</option>
-            <option value='09'>09</option>
-            <option value='10'>10</option>
-            <option value='11'>11</option>
-            <option value='12'>12</option>
-            <option value='13'>13</option>
-          </select>
-          <br />
-          <button type='button' onClick={createZip}>
-            Export Preset
-          </button>
-
-          <button type='button' onClick={toggleModal}>
-            Cancel
-          </button>
-        </ModalContent>
-      </PresetModal>
     </>
   );
 };
